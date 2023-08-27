1891db335be8ecea42e445272e238bfdb4af2237
BKCHAITANYA <bkchaitanya05@gmail.com>
2023-08-27 12:12:11 +0530

    first

--- 01-Hello-world.sh
+++ 01-Hello-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "...........................Hello world..............."
-P1=ramesh
-P2=suresh
+P1=rameshhhh
+P2=sureshhhh
 echo "$P1: hai $P2.............Hello world........"
 echo "$P2: I am good $P1.............Hello world........"
