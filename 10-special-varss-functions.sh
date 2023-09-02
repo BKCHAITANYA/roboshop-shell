597a322ba4f4c6996237a59cb3275b663524ec3c
BKCHAITANYA <bkchaitanya05@gmail.com>
2023-09-02 20:10:51 +0530

    first

--- 10-special-varss-functions.sh
+++ 10-special-varss-functions.sh
@@ -22,8 +22,8 @@ if [ $USERID -ne 0 ]
 then
     echo "Error: Please run the script with root access"
     exit 1
-else 
-    echo "you are root user"
+#else 
+#    echo "you are root user"
 fi
 
 # it is our responsibility to ensure that installation is success or not
